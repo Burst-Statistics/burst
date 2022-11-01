538e123f9e7720e308b9c1ed5db5942f787b2849
Hessel de Jong <info@hesseldejong.com>
2022-11-01 10:55:25 +0100

    Updated for React

--- burst.php
+++ burst.php
@@ -53,137 +53,131 @@ if ( ! function_exists( 'burst_activation_check' ) ) {
 	register_activation_hook( __FILE__, 'burst_activation_check' );
 }
 
-require_once( plugin_dir_path( __FILE__ ) . 'functions.php' );
-if ( ! class_exists( 'BURST' ) ) {
-	class BURST {
-		public static $instance;
-		public static $endpoint;
-        public static $anonymize_IP;
-		public static $statistics;
-        public static $sessions;
-        public static $goals;
-		public static $admin;
-		public static $settings;
-		public static $progress;
-        public static $frontend;
-        public static $wizard;
-		public static $review;
-		public static $field;
-		public static $config;
-		public static $tour;
-		public static $notices;
+class BURST {
+	private static $instance;
+	public $endpoint;
+	public $anonymize_IP;
+	public $statistics;
+	public $sessions;
+	public $goals;
+	public $admin;
+	public $settings;
+	public $frontend;
+	public $wizard;
+	public $review;
+	public $config;
+	public $tour;
+	public $notices;
 
-		private function __construct() {
-			self::setup_constants();
-			self::includes();
-			self::hooks();
-			self::$endpoint = new burst_endpoint();
-			self::$anonymize_IP = new burst_ip_anonymizer();
-			self::$statistics  = new burst_statistics();
-            self::$sessions  = new burst_sessions();
-            self::$goals  = new burst_goals();
-			self::$config = new burst_config();
-            self::$frontend     = new burst_frontend();
-			if ( is_admin() || wp_doing_cron() ) {
-				self::$admin     = new burst_admin();
-			}
+	private function __construct() {
+	}
+
+	public static function instance() {
+		if ( ! isset( self::$instance ) && ! ( self::$instance instanceof BURST ) ) {
+			self::$instance = new BURST;
+			self::$instance->setup_constants();
+			self::$instance->includes();
+
+			self::$instance->endpoint     = new burst_endpoint();
+			self::$instance->anonymize_IP = new burst_ip_anonymizer();
+			self::$instance->statistics   = new burst_statistics();
+			self::$instance->sessions     = new burst_sessions();
+			self::$instance->goals        = new burst_goals();
+			self::$instance->frontend     = new burst_frontend();
+
 			if ( burst_is_logged_in_rest() || is_admin() || wp_doing_cron() || is_multisite() ) {
-				self::$progress  = new burst_progress();
-				self::$review    = new burst_review();
-				self::$field     = new burst_field();
-				self::$tour      = new burst_tour();
-				self::$notices   = new burst_notices();
+				self::$instance->admin   = new burst_admin();
+				self::$instance->review  = new burst_review();
+				self::$instance->tour    = new burst_tour();
+				self::$instance->notices = new burst_notices();
 			}
+			self::$instance->hooks();
 		}
 
-		/**
-		 * Setup constants for the plugin
-		 */
-
-		private function setup_constants() {
-            define('BURST_STEP_SELECT', 1);
-            define('BURST_STEP_METRICS', 2);
-            define('BURST_STEP_START', 3);
-            define( 'burst_free', true );
-			define( 'burst_url', plugin_dir_url( __FILE__ ) );
-			define( 'burst_path', plugin_dir_path( __FILE__ ) );
-			define( 'burst_plugin', plugin_basename( __FILE__ ) );
-			define( 'burst_plugin_name', 'Burst Statistics' );
-			$debug = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? time() : '';
-			define( 'burst_version', '1.2.1' . $debug );
-			define( 'burst_plugin_file', __FILE__ );
-			define( 'burst_main_menu_position', 100 );
-			define( 'burst_premium_url', 'https://burst-statistics.com/premium?src=burst-plugin' );
-		}
-
-		/**
-		 * Instantiate the class.
-		 *
-		 * @return BURST
-		 * @since 1.0.0
-		 *
-		 */
-		public static function get_instance() {
-			if ( ! isset( self::$instance )
-			     && ! ( self::$instance instanceof BURST )
-			) {
-				self::$instance = new self();
-			}
-
-			return self::$instance;
-		}
-
-		private function includes() {
-			require_once( burst_path . 'integrations/integrations.php');
-			require_once( burst_path . '/class-endpoint.php' );
-			require_once( burst_path . '/tracking/tracking.php' );
-            require_once( burst_path . '/class-frontend.php' );
-			if ( is_admin() || wp_doing_cron() ) {
-				require_once( burst_path . '/class-admin.php' );
-			}
-			if ( burst_is_logged_in_rest() || is_admin() || wp_doing_cron() || is_multisite() ) {
-				require_once( burst_path . 'settings/settings.php' );
-				require_once( burst_path . '/assets/icons.php');
-				require_once( burst_path . '/class-field.php');
-				require_once( burst_path . '/grid/grid.php' );
-				require_once( burst_path . '/class-review.php' );
-				require_once( burst_path . 'progress/class-progress.php');
-				require_once( burst_path . '/shepherd/tour.php' );
-				require_once( burst_path . '/config/notices.php' );
-				if ( isset($_GET['install_pro'])) {
-					require_once( burst_path . 'upgrade/upgrade-to-pro.php' );
-				}
-			}
-            require_once( burst_path . '/helpers/anonymize-ip.php' );
-            require_once( burst_path . '/helpers/php-user-agent/UserAgentParser.php' );
-			require_once( burst_path . '/statistics/class-statistics.php' );
-            require_once( burst_path . '/sessions/class-sessions.php' );
-            require_once( burst_path . '/goals/class-goals.php' );
-			require_once( burst_path . '/config/class-config.php');
-			require_once( burst_path . '/cron/cron.php');
-		}
-
-		private function hooks() {
-			$plugin = plugin_basename(__FILE__);
-
-			/**
-			 * Tell the consent API we're following the api
-			 */
-			add_filter("wp_consent_api_registered_$plugin", function(){return true;});
-		}
+		return self::$instance;
 	}
 
 	/**
-	 * Load the plugins main class.
+	 * Setup constants for the plugin
 	 */
-	add_action(
-		'plugins_loaded',
-		function () {
-			BURST::get_instance();
-		},
-		9
-	);
+
+	private function setup_constants() {
+		define( 'burst_free', true );
+		define( 'burst_url', plugin_dir_url( __FILE__ ) );
+		define( 'burst_path', plugin_dir_path( __FILE__ ) );
+		define( 'burst_plugin', plugin_basename( __FILE__ ) );
+		define( 'burst_plugin_name', 'Burst Statistics' );
+		$debug = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? time() : '';
+		define( 'burst_version', '1.2.2.1' . $debug );
+		define( 'burst_plugin_file', __FILE__ );
+		define( 'burst_main_menu_position', 100 );
+		define( 'burst_premium_url', 'https://burst-statistics.com/premium?src=burst-plugin' );
+	}
+
+	/**
+	 * Instantiate the class.
+	 *
+	 * @return BURST
+	 * @since 1.0.0
+	 *
+	 */
+	public static function get_instance() {
+		if ( ! isset( self::$instance )
+		     && ! ( self::$instance instanceof BURST )
+		) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	private function includes() {
+		require_once( burst_path . 'functions.php' );
+		require_once( burst_path . 'integrations/integrations.php');
+		require_once( burst_path . '/class-endpoint.php' );
+		require_once( burst_path . '/tracking/tracking.php' );
+		require_once( burst_path . '/class-frontend.php' );
+		require_once( burst_path . '/helpers/anonymize-ip.php' );
+		require_once( burst_path . '/helpers/php-user-agent/UserAgentParser.php' );
+		require_once( burst_path . '/statistics/class-statistics.php' );
+		require_once( burst_path . '/sessions/class-sessions.php' );
+		require_once( burst_path . '/goals/class-goals.php' );
+		require_once( burst_path . '/cron/cron.php');
+
+		if ( burst_is_logged_in_rest() || is_admin() || wp_doing_cron() || is_multisite() ) {
+			require_once( burst_path . '/class-admin.php' );
+			require_once( burst_path . 'settings/settings.php' );
+			require_once( burst_path . '/class-review.php' );
+			require_once( burst_path . '/shepherd/tour.php' );
+			require_once( burst_path . '/class-notices.php' );
+			require_once( burst_path . 'class-installer.php');
+			if ( isset($_GET['install_pro'])) {
+				require_once( burst_path . 'upgrade/upgrade-to-pro.php' );
+			}
+		}
+
+
+		// require_once( burst_path . '/pro/includes.php' );
+	}
+
+	private function hooks() {
+		$plugin = plugin_basename(__FILE__);
+
+		/**
+		 * Tell the consent API we're following the api
+		 */
+		add_filter("wp_consent_api_registered_$plugin", function(){return true;});
+	}
 }
+
+function BURST()
+{
+	global $wp_version;
+	if ( version_compare($wp_version, '4.9', '>=') && version_compare(PHP_VERSION, '7.2', '>=')) {
+		return BURST::instance();
+	}
+}
+add_action('plugins_loaded', 'BURST', 8);
 
 if ( ! function_exists( 'burst_set_activation_time_stamp' ) ) {
 	/**
@@ -218,7 +212,6 @@ if ( !function_exists( 'burst_clear_scheduled_hooks' )) {
 		wp_clear_scheduled_hook( 'burst_every_day_hook' );
 	}
 }
-
 if ( ! function_exists('burst_add_view_capability')){
 	/**
 	 * Add a user capability to WordPress and add to admin and editor role
@@ -254,12 +247,6 @@ if ( ! function_exists('burst_add_manage_capability')){
 
 	register_activation_hook( __FILE__, 'burst_add_manage_capability' );
 }
-
-function BURST()
-{
-	return BURST::get_instance();
-}
-add_action('plugins_loaded', 'BURST', 8);
 
 if ( ! function_exists( 'burst_is_logged_in_rest' ) ) {
 	function burst_is_logged_in_rest() {
