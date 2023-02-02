8125f7ced0707db69549e6feed23a76042cbba43
Hessel de Jong <info@hesseldejong.com>
2023-02-02 18:50:35 +0100

    Fixed timezone issue where client timezone was interfering with the datepicker

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '1ff8762e2672e0f61d9c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '7350790ccd87057fb6fe');
