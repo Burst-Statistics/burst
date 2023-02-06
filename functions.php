9c245ab6527546c6ba85ce92aff3fa258efde87a
Hessel de Jong <info@hesseldejong.com>
2023-02-06 10:47:47 +0100

    cmplz prefix changed to burst prefix

--- functions.php
+++ functions.php
@@ -56,7 +56,7 @@ if ( ! function_exists('burst_add_manage_capability')){
 	}
 }
 
-if ( !function_exists('cmplz_add_role_to_subsite') ) {
+if ( !function_exists('burst_add_role_to_subsite') ) {
     /**
      * When a new site is added, add our capability
      *
@@ -64,12 +64,12 @@ if ( !function_exists('cmplz_add_role_to_subsite') ) {
      *
      * @return void
      */
-    function cmplz_add_role_to_subsite( $site ) {
+    function burst_add_role_to_subsite( $site ) {
         switch_to_blog( $site->blog_id );
         burst_add_manage_capability( false );
         restore_current_blog();
     }
-    add_action('wp_initialize_site', 'cmplz_add_role_to_subsite', 10, 1);
+    add_action('wp_initialize_site', 'burst_add_role_to_subsite', 10, 1);
 }
 
 if ( ! function_exists( 'burst_user_can_view' ) ) {
