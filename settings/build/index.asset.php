1756f07d95eea9f571a579feb85501522616f94c
Hessel de Jong <info@hesseldejong.com>
2023-02-08 14:48:46 +0100

    Run build

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'ca30cc66f9babf9364f7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'b995ac3ec06eb8a14f25');
