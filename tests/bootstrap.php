c164a18464e63ccdf4f1b97dbac0b07459d80d03
Hessel de Jong <info@hesseldejong.com>
2023-02-08 15:36:02 +0100

    Fixed tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 /**
  * PHPUnit bootstrap file.
  *
- * @package Burst_Premium
+ * @package Burst
  */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
@@ -23,7 +23,7 @@ require_once "{$_tests_dir}/includes/functions.php";
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( __FILE__, 2 ) . '/burst-premium.php';
+	require dirname( __FILE__, 2 ) . '/burst.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
