902bdbe47e0ba2ea5ff612406e067f570d0a567a
Hessel de Jong <info@hesseldejong.com>
2023-05-03 18:49:06 +0200

    Internalization

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '616df29ffc01e3986c01');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '434a24d55d617fe9b17a');
--- settings/src/components/blocks/Goals/DeleteGoalModal.js
+++ settings/src/components/blocks/Goals/DeleteGoalModal.js
@@ -2,26 +2,27 @@ import Modal from '../../common/Modal';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import { formatUnixToDate } from '../../../utils/formatting';
+import {__} from '@wordpress/i18n';
 
 const DeleteGoalModal = ({ isOpen, goal, onDelete, onClose }) => {
   return (
       <Modal isOpen={isOpen} title="Delete Goal" onClose={onClose}>
         <DialogContent>
-          <p>Are you sure you want to delete this goal? This action cannot be undone.</p>
+          <p>{__('Are you sure you want to delete this goal? This action cannot be undone.', 'burst-statistics')}</p>
           <p>
-            <strong>Goal Name:</strong> {goal.name}
+            <strong>{__('Goal name', 'burst-statistics')}:</strong> {goal.name}
             <br />
-            <strong>Status:</strong> {goal.status}
+            <strong>{__('Status', 'burst-statistics')}:</strong> {goal.status}
             <br />
-            <strong>Date Created:</strong> {formatUnixToDate(goal.dateCreated)}
+            <strong>{__('Date created', 'burst-statistics')}:</strong> {formatUnixToDate(goal.dateCreated)}
           </p>
         </DialogContent>
         <DialogActions>
           <button className={"burst-button burst-button--secondary"} onClick={onClose}>
-            Cancel
+              {__('Cancel', 'burst-statistics')}
           </button>
           <button className={"burst-button burst-button--tertiary"} onClick={onDelete}>
-            Delete Goal
+              {__('Delete goal', 'burst-statistics')}
           </button>
         </DialogActions>
       </Modal>
