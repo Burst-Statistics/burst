caab2856b7d96302c130d7d685df02400460988c
Hessel de Jong <info@hesseldejong.com>
2023-07-07 11:25:05 +0200

    Revert "Decoded URL's for languages like Persian"
    This reverts commit d47044f8533cbd43c4b248e510c5811da5193314.

--- settings/build/index.2cf66bf28927ad3e6bae.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '2cf66bf28927ad3e6bae');
---
+++ settings/build/index.59e38c9d9bde9aaa0025.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '59e38c9d9bde9aaa0025');
