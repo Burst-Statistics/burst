140aa5fa42ca36cf99b1f84c9bf2aea2a8566dc8
Rogier Lankhorst <rogierlankhorst@gmail.com>
2024-02-20 16:06:51 +0100

    shortcode

--- class-frontend.php
+++ class-frontend.php
@@ -17,12 +17,79 @@ if ( ! class_exists( "burst_frontend" ) ) {
             add_action('admin_bar_menu', array($this, 'add_to_admin_bar_menu'), 35);
             add_action('admin_bar_menu', array($this, 'add_top_bar_menu'), 400 );
 	        add_action( 'init', array( $this, 'register_pageviews_block' ) );
-		}
+
+	        add_shortcode( 'burst-most-visited', array( $this, 'most_visited_posts' ) );
+
+        }
 
         static function this()
         {
             return self::$_this;
         }
+
+	    /**
+	     * Show content conditionally, based on consent
+	     * @param array  $atts
+	     * @param string   $content
+	     * @param string $tag
+	     *
+	     * @return false|string
+	     */
+
+	    public function most_visited_posts(
+		    $atts = array(), $content = null, $tag = ''
+	    ) {
+		    // normalize attribute keys, lowercase
+		    $atts = array_change_key_case( (array) $atts, CASE_LOWER );
+		    $blocked_text = __('Click to accept the cookies for this service', 'complianz-gdpr');
+		    // override default attributes with user attributes
+		    $atts   = shortcode_atts( array(
+			    'count'       => 5,
+			    'post_type'   => 'post',
+			    'show_count'   => false,
+		    ), $atts, $tag );
+
+		    //sanitize post type
+		    $post_types = get_post_types();
+			if ( !in_array( $atts['post_type'], $post_types ) ) {
+			    $atts['post_type'] = 'post';
+		    }
+
+		    $count = (int) $atts['count'];
+		    $show_count = (bool) $atts['show_count'];
+		    $post_type = (string) $atts['post_type'];
+			//posts, sorted by post_meta
+		    $args = [
+			    'post_type'         => $post_type,
+			    'numberposts'       => $count,
+			    'meta_key'          => 'burst_total_pageviews_count',
+			    'orderby'           => 'meta_value_num',
+			    'order'             => 'DESC',
+			    'meta_query'        => array(
+				    'key'   => 'burst_total_pageviews_count', // Same meta key for sorting
+				    'type'  => 'NUMERIC' // Make sure to specify the type as numeric for correct sorting
+			    )
+		    ];
+		    $posts = get_posts($args);
+		    ob_start();
+
+		    if (count($posts)>0){
+				?>
+			    <ul class="burst-most-visited-posts">
+				    <?php foreach ($posts as $post) {
+						$count = (int) get_post_meta($post->ID, 'burst_total_pageviews_count', true);
+					    $count_html = '';
+						if ($show_count) {
+							$count_html = '&nbsp;<span class="burst-post-count">(' . apply_filters('burst_most_visited_count', $count, $post ). ')</span>';
+						}
+						?>
+					    <li class="burst-post-item"><a href="<?php esc_url_raw(get_the_permalink($post))?>"><?php echo esc_html(get_the_title($post)); ?><?php echo $count_html?></a></li>
+	                <?php }; ?>
+			    </ul>
+				<?php
+			}
+		    return ob_get_clean();
+	    }
 
         public function add_to_admin_bar_menu( $wp_admin_bar ) {
             if ( ! burst_user_can_view() || is_admin() ) {
