6aae551c5f0ff3afeb69fe5966af2d3a29eddd1e
Hessel de Jong <info@hesseldejong.com>
2024-04-02 19:13:58 +0200

    Synced changes from Burst Pro: Change-upgrade-button-to-Pro

--- settings/src/components/blocks/Fields/Pro.js
+++ settings/src/components/blocks/Fields/Pro.js
@@ -4,19 +4,19 @@ import { __ } from '@wordpress/i18n';
  * Render a premium tag
  */
 const Pro = ({pro, id}) => {
-    if ( burst_settings.is_pro || !pro ) {
-        return null
+    if ( burst_settings.is_pro || ! pro ) {
+        return null;
     }
 
-    let url = pro.url ? pro.url : 'https://burst-statistics.io/pricing';
-    url+='?src='+id;
+    let url = pro.url ? pro.url : 'https://burst-statistics.com/pricing';
+    url += '?src=' + id;
     return (
         <div className="burst-pro">
-            <a target="_blank" href={url}>{__("Upgrade", "burst-statistics")}</a>
+            <a target="_blank" href={url} rel="noreferrer">{__( 'Pro', 'burst-statistics' )}</a>
         </div>
 
     );
 
-}
+};
 
-export default Pro
+export default Pro;
